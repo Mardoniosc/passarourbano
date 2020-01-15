e80d8d63f8ca43bc37e1552b1e2f0288bb988dbd
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-01-15 09:13:39 -0300

    iniciou-se o estudo sobre observables

--- src/app/components/oferta/oferta.component.ts
+++ src/app/components/oferta/oferta.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute } from '@angular/router'
 import { OfertasService } from '../../services/ofertas.service'
 import { Oferta } from '../../shared/model/oferta.model'
 
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/Rx'
+
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -25,6 +28,19 @@ export class OfertaComponent implements OnInit {
         this.oferta = oferta
         console.log(oferta)
        })
+
+    // this.route.params.subscribe(
+    //   (parametro: any) => {console.log(parametro)},
+    //   (erro: any) => console.log(erro),
+    //   (() => console.log('Processamento comcluído!'))
+
+    //   )
+
+    let tempo = Observable.interval(2000)
+
+    tempo.subscribe((intervalo: number) => {
+      console.log(intervalo)
+    })
   }
 
 }
